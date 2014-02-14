fbb64aa867b39342b7b1d4bde3ca609741013fe7
Larry Davis <lazdnet@gmail.com>
2014-02-13 18:46:55 -0700

    Convert tabs to spaces

--- index.js
+++ index.js
@@ -5,159 +5,159 @@ var count = 0;
 var variableRE = /\{\{(.*?)\}\}/g;
 
 function getVariableArray(string) {
-	var array = [];
-	var lastOffset = 0;
-	string.replace(variableRE, function(match, p1, offset, string) {
-		// Add intermediate text
-		var text = string.slice(lastOffset, offset);
-		if (text.length) {
-			array.push(text);
-		}
+  var array = [];
+  var lastOffset = 0;
+  string.replace(variableRE, function(match, p1, offset, string) {
+    // Add intermediate text
+    var text = string.slice(lastOffset, offset);
+    if (text.length) {
+      array.push(text);
+    }
 
-		array.push({ variable: p1 });
+    array.push({ variable: p1 });
 
-		lastOffset = offset + match.length;
+    lastOffset = offset + match.length;
 
-		return match;
-	});
+    return match;
+  });
 
-	if (lastOffset !== string.length) {
-		array.push(string.slice(lastOffset));
-	}
+  if (lastOffset !== string.length) {
+    array.push(string.slice(lastOffset));
+  }
 
-	return array;
+  return array;
 }
 
 function makeVariableExpression(string) {
-	if (!usesVariables(string)) {
-		return safe(string);
-	}
+  if (!usesVariables(string)) {
+    return safe(string);
+  }
 
-	var expression = '';
-	var pieces = getVariableArray(string);
-	pieces.forEach(function(piece, index) {
-		if (index !== 0) {
-			expression += '+';
-		}
+  var expression = '';
+  var pieces = getVariableArray(string);
+  pieces.forEach(function(piece, index) {
+    if (index !== 0) {
+      expression += '+';
+    }
 
-		if (typeof piece === 'string') {
-			expression += safe(piece);
-		}
-		else {
-			expression += 'data['+safe(piece.variable)+']';
-		}
-	});
+    if (typeof piece === 'string') {
+      expression += safe(piece);
+    }
+    else {
+      expression += 'data['+safe(piece.variable)+']';
+    }
+  });
 
-	return expression;
+  return expression;
 }
 
 function safe(string) {
-	return JSON.stringify(string);
+  return JSON.stringify(string);
 }
 
 function getElName() {
-	return 'el'+(count++);
+  return 'el'+(count++);
 }
 
 function createElement(elName, tag, elHandle) {
-	var statement = 'var '+elName+' = ';
-	var handleUsesDollar;
-	var handleProperty;
-	var elHandleBare;
-	var handleProperty;
+  var statement = 'var '+elName+' = ';
+  var handleUsesDollar;
+  var handleProperty;
+  var elHandleBare;
+  var handleProperty;
 
-	if (elHandle) {
-		handleUsesDollar = elHandle.charAt(0) === '$';
-		elHandleBare = handleUsesDollar ? elHandle.slice(1) : elHandle;
-		handleProperty = 'this['+safe(elHandleBare)+']';
-	}
+  if (elHandle) {
+    handleUsesDollar = elHandle.charAt(0) === '$';
+    elHandleBare = handleUsesDollar ? elHandle.slice(1) : elHandle;
+    handleProperty = 'this['+safe(elHandleBare)+']';
+  }
 
-	if (elHandle) {
-		statement += handleProperty+' = ';
-	}
-	statement += 'document.createElement("'+tag+'");\n';
+  if (elHandle) {
+    statement += handleProperty+' = ';
+  }
+  statement += 'document.createElement("'+tag+'");\n';
 
-	if (elHandle && handleUsesDollar) {
-		statement += 'this['+safe(elHandle)+'] = $('+elName+');\n';
-	}
+  if (elHandle && handleUsesDollar) {
+    statement += 'this['+safe(elHandle)+'] = $('+elName+');\n';
+  }
 
-	return statement;
+  return statement;
 }
 
 function setAttribute(elName, attr, value) {
-	return elName+'.setAttribute('+safe(attr)+', '+makeVariableExpression(value)+');\n'
+  return elName+'.setAttribute('+safe(attr)+', '+makeVariableExpression(value)+');\n'
 }
 
 function setTextContent(elName, text) {
-	return elName+'.textContent = '+makeVariableExpression(text)+';\n';
+  return elName+'.textContent = '+makeVariableExpression(text)+';\n';
 }
 
 function usesVariables(string) {
-	return string.match(variableRE);
+  return string.match(variableRE);
 }
 
 function createTextNode(elName, text) {
-	return 'var '+elName+' = document.createTextNode('+makeVariableExpression(text)+');\n';
+  return 'var '+elName+' = document.createTextNode('+makeVariableExpression(text)+');\n';
 }
 
 function buildFunctionBody($, el, parentName) {
-	var func = '';
+  var func = '';
 
-	el.children.forEach(function(el, index) {
-		var elName = getElName();
-		if (el.type === 'tag') {
-			func += createElement(elName, el.name, el.attribs['data-handle']);
+  el.children.forEach(function(el, index) {
+    var elName = getElName();
+    if (el.type === 'tag') {
+      func += createElement(elName, el.name, el.attribs['data-handle']);
 
-			var attrs = el.attribs;
-			for (var attr in attrs) {
-				// Skip internal handles
-				if (attr === 'data-handle') {
-					continue;
-				}
-				func += setAttribute(elName, attr, attrs[attr]);
-			}
+      var attrs = el.attribs;
+      for (var attr in attrs) {
+        // Skip internal handles
+        if (attr === 'data-handle') {
+          continue;
+        }
+        func += setAttribute(elName, attr, attrs[attr]);
+      }
 
-			var children = el.children;
-			if (children.length) {
-				func += buildFunctionBody($, el, elName);
-			}
-			else {
-				var text = $(el).text();
-				if (text.length) {
-					// Set text content directly if there are no children
-					func += setTextContent(elName, text);
-				}
-			}
-		}
-		else if (el.type === 'text') {
-			var text = $(el).text();
-			func += createTextNode(elName, text);
-		}
+      var children = el.children;
+      if (children.length) {
+        func += buildFunctionBody($, el, elName);
+      }
+      else {
+        var text = $(el).text();
+        if (text.length) {
+          // Set text content directly if there are no children
+          func += setTextContent(elName, text);
+        }
+      }
+    }
+    else if (el.type === 'text') {
+      var text = $(el).text();
+      func += createTextNode(elName, text);
+    }
 
-		if (parentName) {
-			func += parentName+'.appendChild('+elName+');\n';
-		}
-	});
+    if (parentName) {
+      func += parentName+'.appendChild('+elName+');\n';
+    }
+  });
 
-	return func;
+  return func;
 }
 
 function compile(html) {
-	// Reset count
-	count = 0;
+  // Reset count
+  count = 0;
 
-	var $ = cheerio.load('<div id="__template-root__">'+html+'</div>');
+  var $ = cheerio.load('<div id="__template-root__">'+html+'</div>');
 
-	var root = $('#__template-root__')[0];
+  var root = $('#__template-root__')[0];
 
-	var functionBody = buildFunctionBody($, root);
+  var functionBody = buildFunctionBody($, root);
 
-	if (root.children.length === 1) {
-		// Return the root element, if there's only one
-		functionBody += 'return el0;\n';
-	}
+  if (root.children.length === 1) {
+    // Return the root element, if there's only one
+    functionBody += 'return el0;\n';
+  }
 
-	return new Function('data', functionBody);
+  return new Function('data', functionBody);
 }
 
 module.exports = compile;
