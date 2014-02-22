44f64a16307b9db12f61854b52235d713daa0952
Larry Davis <lazdnet@gmail.com>
2014-02-22 10:29:42 -0800

    Remove grunt-jison

--- gruntfile.js
+++ gruntfile.js
@@ -137,7 +137,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-jison');
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-simple-mocha');
   grunt.registerTask('build', [ 'jshint', 'compile', 'handlebars', 'dot', 'jst' ]);
