4e73f9852a24f03685f539f578c1c45ca25fba90
Larry Davis <lazdnet@gmail.com>
2014-07-17 14:05:27 +0200

    Basic string and number literal support (does not support escaping), closes #10

--- lib/grammar-statement.jison
+++ lib/grammar-statement.jison
@@ -4,15 +4,20 @@
 %lex
 %%
 
-[\w\d_$]+\b   return 'VAR'
-"("           return '('
-")"           return ')'
-"."           return '.'
-","           return ','
-";"           return ';'
-\s+           /* skip whitespace */
-<<EOF>>       return 'EOF'
-.             return 'INVALID'
+[\w\d_$]+\b           return 'VAR'
+[-+]?[0-9]*\.?[0-9]+  return 'NUMBER'
+\'[^']+\'             return 'SINGLE_QUOTED_STRING'
+\"[^"]+\"             return 'DOUBLE_QUOTED_STRING'
+"\""                  return 'DOUBLE_QUOTE'
+"'"                   return 'SINGLE_QUOTE'
+"("                   return '('
+")"                   return ')'
+"."                   return '.'
+","                   return ','
+";"                   return ';'
+\s+                   /* skip whitespace */
+<<EOF>>               return 'EOF'
+.                     return 'INVALID'
 
 /lex
 
@@ -31,9 +36,25 @@ invocation
   : path '(' args ')' -> { type: 'invocation', path: $path, args: $args };
   ;
 
+stringLiteral
+  : SINGLE_QUOTED_STRING
+  | DOUBLE_QUOTED_STRING
+  ;
+
+numberLiteral
+  : NUMBER
+  ;
+
+literal
+  : stringLiteral -> { type: 'literal', value: $stringLiteral };
+  | numberLiteral -> { type: 'literal', value: $numberLiteral };
+  ;
+
 args
   : path -> $$ = [$1];
-  | invocation -> $$ = [$1]
+  | invocation -> $$ = [$1];
+  | literal -> $$ = [$1];
+  | args ',' literal -> $1.push($3); $$ = $1;
   | args ',' path -> $1.push($3); $$ = $1;
   |
   ;
---
+++ test/Literals.js
@@ -0,0 +1,82 @@
+var expect = require('chai').expect;
+var test = require('./lib/test.js');
+
+describe.only('Literals', function() {
+  it('should allow a double-quoted string literal as the only argument', function() {
+    test({
+      fixture: 'Literals - Double-quoted string',
+      done: function($) {
+        expect($('p').text()).to.equal('meow');
+      },
+      data: {
+        passThrough: function(string) {
+          return string;
+        }
+      }
+    });
+  });
+
+  it('should allow a single-quoted string literal as the only argument', function() {
+    test({
+      fixture: 'Literals - Single-quoted string',
+      done: function($) {
+        expect($('p').text()).to.equal('meow');
+      },
+      data: {
+        passThrough: function(string) {
+          return string;
+        }
+      }
+    });
+  });
+
+  it('should allow multiple double and single-quoted string as arguments', function() {
+    test({
+      fixture: 'Literals - Double and single-quoted string arguments',
+      done: function($) {
+        expect($('p').text()).to.equal('meowmoocluck');
+      },
+      data: {
+        concat: function() {
+          var string = '';
+          for (var i = 0; i < arguments.length; i++) {
+            string += arguments[i];
+          }
+          return string;
+        }
+      }
+    });
+  });
+
+  it('should allow a number literal as the only argument', function() {
+    test({
+      fixture: 'Literals - Number',
+      done: function($) {
+        expect($('p').text()).to.equal('42');
+      },
+      data: {
+        passThrough: function(string) {
+          return string;
+        }
+      }
+    });
+  });
+
+  it('should allow multiple number literals as arguments', function() {
+    test({
+      fixture: 'Literals - Numbers',
+      done: function($) {
+        expect($('p').text()).to.equal('-0.75');
+      },
+      data: {
+        sum: function() {
+          var total = 0;
+          for (var i = 0; i < arguments.length; i++) {
+            total += arguments[i];
+          }
+          return total;
+        }
+      }
+    });
+  });
+});
